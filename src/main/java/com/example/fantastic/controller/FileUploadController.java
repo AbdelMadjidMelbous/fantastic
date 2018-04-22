5b95f269a5b40ecbdbc7cd10bde9b58947fe71ea
Sid Ahmed Hamani <sid_hamani@MacBook-Air-de-Sid.local>
2018-04-22 11:16:23 +0100

    Adding the following features:File Dowloading, File Deleting

--- src/main/java/com/example/fantastic/controller/FileUploadController.java
+++ src/main/java/com/example/fantastic/controller/FileUploadController.java
@@ -6,14 +6,17 @@ import com.example.fantastic.repository.SupportDeCoursRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.xml.bind.DatatypeConverter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,6 +28,8 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import org.springframework.core.io.ByteArrayResource;
+
 
 @Controller
 
@@ -39,6 +44,41 @@ public class FileUploadController {
     {
         folderPath = "src/main/resources/static/cours/";
     }
+
+    @RequestMapping(value = "/cours", method = RequestMethod.GET)
+    public String cours(Model model){
+        model.addAttribute("cours", coursRepository.findAll());
+        return "Enfant/ListeFichier";
+
+    }
+
+    @RequestMapping(value = "/cours/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> TelechargerUnFichier(@PathVariable Long id, Model model){
+        System.out.println(id);
+        SupportDeCours cours=coursRepository.findById(id).get();
+        String fileName=cours.getHachedName();
+        System.out.println(fileName);
+        try {
+            return download(fileName);
+        }
+        catch (IOException e){
+            System.out.println(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/supprimerCours/{id}", method = RequestMethod.GET)
+    public String SupprimerUnFichier(@PathVariable Long id, Model model){
+        System.out.println("id value ="+id);
+        SupportDeCours cours=coursRepository.findById(id).get();
+        String fileName=cours.getHachedName();
+        deleteFile(fileName);
+        coursRepository.deleteById(id);
+        return "redirect:/cours";
+    }
+
+
+
     @RequestMapping(value = "/api/fileupload", method = RequestMethod.POST)
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile,@RequestParam("niveau_cours") Long niveau_id) {
         //logger.debug("Single file upload!");
@@ -53,7 +93,10 @@ public class FileUploadController {
                 DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
                 Date date = new Date();
                 String fullName=uploadfile.getOriginalFilename()+date.toString();//en cas de plusieurs fichiers avec le meme nom, comme ça on s'assure qu'il y aura pas le meme nom(on a ajouté la date aussi)
-                String hachCode=generateName(fullName);
+                String hach=generateName(fullName);
+                String[] fileFrags = uploadfile.getOriginalFilename().split("\\.");
+                String extension = fileFrags[fileFrags.length-1];
+                String hachCode=hach+"."+extension;
                 saveUploadedFiles(Arrays.asList(uploadfile),hachCode);
                 saveFileInBDD(uploadfile.getOriginalFilename(),hachCode,date,niveau_id);
                 //Ajout du nom du fichier dans la BDD
@@ -82,7 +125,8 @@ public class FileUploadController {
             }
 
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(""+folderPath+hachCode+file.getOriginalFilename());
+
+            Path path = Paths.get(""+folderPath+hachCode);
             System.out.println(path.toAbsolutePath().toString());
             Files.write(path, bytes);
 
@@ -109,6 +153,41 @@ public class FileUploadController {
                 .printHexBinary(digest).toUpperCase();
 
         return myname;
+    }
+
+    public ResponseEntity<?> download(String fileName) throws IOException {
+        Path path = Paths.get(""+folderPath+fileName);
+        System.out.println(path.toAbsolutePath().toString());
+//        Path path = Paths.get(folderPath+nom);
+        byte[] data = Files.readAllBytes(path);
+        ByteArrayResource resource = new ByteArrayResource(data);
+
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename=" + path.getFileName().toString())
+                .contentType(MediaType.APPLICATION_PDF).contentLength(data.length)
+                .body(resource);
+
+    }
+    public void deleteFile (String fileName){
+        Path path = Paths.get(""+folderPath+fileName);
+        try{
+
+            File file = new File(path.toAbsolutePath().toString());
+
+            if(file.delete()){
+                System.out.println(file.getName() + " is deleted!");
+            }else{
+                System.out.println("Delete operation has failed.");
+            }
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
     }
 
 }
